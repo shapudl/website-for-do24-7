33cffb5fb56c4b6acd92562fbbdc4b4f02dcf305
shapudl <tena.spoljar@gmail.com>
2015-03-23 20:24:42 +0100

    I hate this background button added

--- dark.js
+++ dark.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-console.log("ready")
+console.log("ready");
 
 
-$("#newbg").click(function(){
-  $("body").css("background",'url("noise.png") pink repeat');
+$("#newbg").click(function() {
+  console.log("inside");
+  $("body").css("background",'url("noise.png") #26D0FF repeat');
+  $("#newbg").text("Are you sure");
 });
 
 
